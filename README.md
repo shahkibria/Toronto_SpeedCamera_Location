d5903e9808ea7eea6dbc6262357240c3d8547260
Shah Kibria <shahkibria@Shahs-MacBook-Air.local>
2021-05-25 13:13:57 -0400

    upload

--- README.md
+++
@@ -1,2 +0,0 @@
-# Toronto_SpeedCamera_Location
-Presenting Speed Camera Locations for new 50 Speed Cameras added by the City on May 25
--- config.js
+++
@@ -1,2 +0,0 @@
-// API key
-const API_KEY = "pk.eyJ1Ijoic2hhaHJhZnNhbjMzIiwiYSI6ImNrb3Q4eWdjejA3djYybm83cWVudWpnMGMifQ.5-O0m3Caub-82s_RlJe_xg";
